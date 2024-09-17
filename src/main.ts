1d25576e04f6d0a4eff78dd2e9c006446a05715b
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2024-09-17 09:04:08 +0330

    configurable port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, '0.0.0.0');
+  const conf = app.get(ConfigService);
+  const port = +(conf.get('PORT') || 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
